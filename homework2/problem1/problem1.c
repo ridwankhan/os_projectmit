173c57dd5931b78c8f9bfd833e6e245323911891
Ridwan Khan <ridwankhan101@gmail.com>
2017-04-01 17:22:09 -0400

    Problem 1 is pretty much ready

--- homework2/problem1/problem1.c
+++ homework2/problem1/problem1.c
@@ -5,6 +5,7 @@
 #include <pthread.h>
 #include <math.h>
 
+/*
 struct myStruct{
 
 	
@@ -12,68 +13,74 @@ struct myStruct{
 	int myArray[];
 
 	};
-
-void sieve(struct myStruct* args){
-
-	printf("here now %d\n", args->number);
-	int root = sqrt(args->number);
-	printf("root %d\n", root);
-	for (int i = 0; i<args->number; i++){
-		
-		printf("array %d\n", args->myArray[i]);
-	}
+*/
+void sieve(int myArray[]){
+	
+	int number = myArray[0];
+	
+	int root = sqrt(myArray[0]);
+	//printf("root %d\n", root);
+	
 
 	for (int i=2; i<=root; i++){
-		printf("hi there ");
-		printf("%d\n", args->myArray[i-1]);
-		if (args->myArray[i-1] == 1){
-			for (int j = i*i; j<=args->number; j= j+i){
-				printf("j number %d\n",j);
-				printf("editing %d\n", j-1);
-				args->myArray[j-1] = 0;
+		
+		if (myArray[i] == 1){
+			for (int j = i*i; j<=(number+1); j= j+i){
+				
+				myArray[j] = 0;
 			}
 		}
 	}
 
-	printf("this index %d\n",args->myArray[6]);
+	
+	for (int i = 2; i<(number+1); i++){
+		if (myArray[i] ==1){
+			printf("Prime Number: %d\n", i);
+		}
+	}
+
 
 }
 
-/*
-void reversePrime(int myArray[], int number){
+
+void reversePrime(int myArray[]){
 	int n;
 	int reverse;
+	int number = myArray[0];
 
-	for (int i = 1; i < number; ++i)
+	for (int i = 2; i < (number+1); ++i)
 	{
 		if (myArray[i] ==1)
 		{
-			n = i+1;
+			n = i;
 			reverse = 0;
 			while (n != 0){
 			reverse = reverse*10;
 			reverse = reverse + n%10;
 			n = n/10;
 			}
-			if ((reverse <= number) && myArray[reverse-1])
+			if ((reverse <= number) && myArray[reverse])
 			{
-				printf("%d\n", i+1);
+				printf("Reverse Primes: %d\n", i);
 			}
 		}
 		
 	}
 	
+	
 }
-*/
+
 
 int main(int argc, char *argv[]){
-	struct myStruct vars;
+	//struct myStruct vars;
 	//args = malloc(sizeof(struct myStruct);
-	vars.number = atoi(argv[1]);
-	vars.myArray[vars.number];
-	for (int i = 0; i<vars.number; i++){
-		vars.myArray[i] = 1;
-		printf("main %d\n", vars.myArray[i]);
+	int number = atoi(argv[1]);
+	int myArray[number+1];
+	myArray[0] = number;
+
+	for (int i = 1; i<(number+1); i++){
+		myArray[i] = 1;
+		//printf("main %d\n", myArray[i]);
 	}
 	
 
@@ -82,26 +89,21 @@ int main(int argc, char *argv[]){
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
 	
-	pthread_create(&tid, &attr, (void*)sieve, &vars);
-
-	//sieve(&vars);
-	//printf("returns\n");
-	//pthread_join(tid,NULL);
-	printf("number %d\n", vars.number );
+	pthread_create(&tid, &attr, (void*)sieve, &myArray);
 	
-	for (int i = 1; i<vars.number; i++){
-		if (vars.myArray[i] ==1){
-			printf("prime %d\n", i+1);
-		}
-	}
-
-	//reversePrime(myArray, number);
-	 
-	for (int i = 0; i<vars.number; i++){
-		
-		printf("main %d\n", vars.myArray[i]);
-	}
-	printf("%d\n", vars.number);
+	//sieve(myArray);
+	//printf("returns\n");
+	pthread_join(tid,NULL);
+	//printf("number %d\n", number );
+	
+	pthread_t tid2;
+	pthread_attr_t attr2;
+	pthread_attr_init(&attr2);
+	
+	pthread_create(&tid2, &attr2, (void*)reversePrime, &myArray);
+	 pthread_join(tid,NULL);
+	
+	
 	
 	return 0;
 	
