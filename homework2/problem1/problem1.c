3273787b9d560024c4b675d827bcebc6d90c8ffc
Ridwan Khan <ridwankhan101@gmail.com>
2017-04-01 14:26:33 -0400

    not working

--- homework2/problem1/problem1.c
+++ homework2/problem1/problem1.c
@@ -5,33 +5,81 @@
 #include <pthread.h>
 #include <math.h>
 
-void sieve(int myArray[],int number){
-	int root = sqrt(number);
-	printf("%d\n", number);
-	printf("%d\n", root);
+struct myStruct{
+
+	
+	int number;
+	int myArray[];
+
+	};
+
+void sieve(struct myStruct args){
+	printf("here now %d\n", args.number);
+	int root = sqrt(args.number);
+	//printf("%d\n", number);
+	//printf("%d\n", root);
 
 	for (int i=2; i<=root; i++){
-		if (myArray[i-1] == 1){
-			for (int j = i*i; j<=number; j= j+i){
+		if (args.myArray[i-1] == 1){
+			for (int j = i*i; j<=args.number; j= j+i){
 				printf("here %d\n",j);
-				myArray[j-1] = 0;
+				args.myArray[j-1] = 0;
 			}
 		}
 	}
 
 }
 
-int main(int argc, char *argv[]){
-	int number = atoi(argv[1]);
-	int myArray[number];
-	for (int i = 0; i<number; i++){
-		myArray[i] = 1;
+void reversePrime(int myArray[], int number){
+	int n;
+	int reverse;
+
+	for (int i = 1; i < number; ++i)
+	{
+		if (myArray[i] ==1)
+		{
+			n = i+1;
+			reverse = 0;
+			while (n != 0){
+			reverse = reverse*10;
+			reverse = reverse + n%10;
+			n = n/10;
+			}
+			if ((reverse <= number) && myArray[reverse-1])
+			{
+				printf("%d\n", i+1);
+			}
+		}
+		
 	}
-	sieve(myArray, number);
-	for (int i = 1; i<number; i++){
-		if (myArray[i] ==1){
+	
+}
+
+int main(int argc, char *argv[]){
+	struct myStruct args;
+	args.number = atoi(argv[1]);
+	args.myArray[args.number];
+	for (int i = 0; i<args.number; i++){
+		args.myArray[i] = 1;
+	}
+
+	printf("pr %d\n", args.number);
+	pthread_t tid;
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	
+	pthread_create(&tid, &attr, (void*)sieve, &args);
+	//sieve(myArray, number);
+	pthread_join(tid,NULL);
+	for (int i = 1; i<args.number; i++){
+		if (args.myArray[i] ==1){
 			printf("prime %d\n", i+1);
 		}
 	}
-	printf("%d\n", number);
+
+	//reversePrime(myArray, number);
+	 
+
+	printf("%d\n", args.number);
+	
 }
